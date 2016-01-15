36fcf80509dddfb49350e3d228ffaa8cbab3aeb8
Stef Bastiaansen <stef.bastiaansen@wijs.be>
2016-01-15 22:18:27 +0100

    Add installation script

---
+++ software.sh
@@ -0,0 +1,64 @@
+# install all the things
+
+# BREW
+brew install figlet
+
+# CASK
+# general
+brew cask install java
+brew cask install disk-inventory-x
+
+# browsers
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+
+# text editors
+brew cask install atom
+brew cask install coda
+brew cask install sublime-text
+brew cask install phpstorm
+
+# database
+brew cask install sequel-pro
+brew cask install psequel
+brew cask install mysqlworkbench
+
+# images
+brew cask install seashore
+
+# dtp
+brew cask install inkscape
+brew cask install scribus
+
+# text
+brew cask install libreoffice
+brew cask install mou
+
+# games
+brew cask install nestopia
+
+# video
+brew cask install vlc
+brew cask install aegisub
+brew cask install mplayerx
+brew cask install handbrake
+
+# misc
+brew cask install alarm-clock
+brew cask install alfred
+brew cask install disk-inventory-x
+brew cask install iterm2
+brew cask install hipchat
+brew cask install github-desktop
+
+# ftp
+brew cask install filezilla
+brew cask install transmission
+brew cask install dropbox
+brew cask install google-drive
+
+
+# not installable
+# armagetron advanced
+# sony bridge for mac
