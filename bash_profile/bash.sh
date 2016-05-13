adf4f0c103564704af8b6b2c5b73e9dc968b1414
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2016-05-13 11:31:41 +0200

    Make a colored prompt

--- bash_profile/bash.sh
+++ bash_profile/bash.sh
@@ -16,6 +16,6 @@ parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-export PS1="\u \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS1="\[\033[33m\]\u\[\033[35m\]@\h\[\033[00m\] \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 
