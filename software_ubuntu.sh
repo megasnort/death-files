130f72e122a7ac8f2cd833796b7d8c8c6baa31d3
Stef Bastiaansen <stef@megasnort.com>
2016-09-16 10:26:02 +0200

    Add git to ubuntu installer and assume yes

--- software_ubuntu.sh
+++ software_ubuntu.sh
@@ -2,33 +2,33 @@
 
 sudo apt-get update
 
-sudo apt-get install htop
-sudo apt-get install docker-engine
+sudo apt-get --assume-yes install git
+sudo apt-get --assume-yes install htop
+sudo apt-get --assume-yes install docker-engine
 
 # python
-sudo apt-get install python-pip
+sudo apt-get --assume-yes install python-pip
+sudo apt-get --assume-yes install python-dev
+sudo apt-get --assume-yes install python3-dev
 sudo pip install virtualenvwrapper
-sudo apt-get install python-dev
-sudo apt-get install python3-dev
-
 
 # postgres
-sudo apt-get install postgresql
-sudo apt-get install postgresql-contrib
-sudo apt-get install python-psycopg2
-sudo apt-get install libpq-dev
+sudo apt-get --assume-yes install postgresql
+sudo apt-get --assume-yes install postgresql-contrib
+sudo apt-get --assume-yes install python-psycopg2
+sudo apt-get --assume-yes install libpq-dev
 
 
-sudo apt-get install memcached
+sudo apt-get --assume-yes install memcached
 
 #redis
-sudo apt-get install build-essential
-sudo apt-get install tcl8.5
-sudo apt-get install redis-server
+sudo apt-get --assume-yes install build-essential
+sudo apt-get --assume-yes install tcl8.5
+sudo apt-get --assume-yes install redis-server
 
 
 # java
-sudo apt-get install openjdk-7-jre
+sudo apt-get --assume-yes install openjdk-7-jre
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
@@ -37,7 +37,7 @@ sudo apt-get -y install oracle-java8-installer
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 sudo apt-get update
-sudo apt-get install elasticsearch
+sudo apt-get --assume-yes install elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
 
 echo "Do not forget to add the virtualenvwrapper script to .bash_profile"
