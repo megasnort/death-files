1db5e1b727f3faac564ca048a855241a473e1389
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2016-09-22 16:15:35 +0200

    postgres user should be allowed to do everything

--- bash_profile/postgres.sh
+++ bash_profile/postgres.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # create a database with the same name and password, of course, only for local development
+
 function mkpostgres() {
-    createuser --pwprompt $1
+    createuser --pwprompt --createrole --superuser $1
     createdb -O$1 -Eutf8 $1
 }
