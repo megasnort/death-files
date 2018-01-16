0912973b72bb1bcbec69e64b804a2889f07f1272
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2018-01-16 17:03:31 +0100

    Remove unused python function

--- bash_profile/python.sh
+++ bash_profile/python.sh
@@ -6,22 +6,6 @@ export WORKON_HOME=~/Envs
 
 
 
-# activate the correct virtualenv on the server, or locally, and cd into the folder
-function ve() {
-    if [ -d ~/Python/$1/ ]; then
-        workon $1
-        cd ~/Python/$1
-    else
-        . /webapps/$1/virtualenv/bin/activate
-
-        if [ -d /webapps/$1/$1/ ]; then
-            cd /webapps/$1/$1
-        else
-            cd /webapps/$1
-        fi
-    fi
-}
-
 # Only on osx: start two terminal windows,
 # one with the running flask dev-server
 # one with the project-root
@@ -67,15 +51,3 @@ alias pm='python manage.py '
 
 # alias for booting ugent web app manager
 alias webapp='workon webapp-management-on-thoth && cd ~/Python/webapp-management-on-thoth/ && fab -l'
-
-# add the currect python version, and pip etc. to the path
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-
-function frameworkpython {
-    if [[ ! -z "$VIRTUAL_ENV" ]]; then
-        PYTHONHOME=$VIRTUAL_ENV ~/.pyenv/shims/python "$@"
-    else
-        ~/.pyenv/shims/python "$@"
-    fi
-}
