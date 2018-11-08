3ca17a21018cc975b9505f484c71ad22067a1296
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2018-11-08 10:56:48 +0100

    Add puall command to push and pull all masters for all repos in the current dir

--- bash_profile/git.sh
+++ bash_profile/git.sh
@@ -3,3 +3,17 @@
 # notice that git autocompletion is located in the seperate .git-completion.bash file
 
 alias g='git'
+
+
+# pull all masters 
+function puall() {
+    for d in */ ; do
+    	cd $d
+    	echo "$d"
+    	if [ -d .git ]; then
+    		git pull origin master
+    		git push origin master
+		fi;
+		cd ..
+	done
+}
