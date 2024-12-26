2aac38f7f99f8fbb4248822547681dde16aa0527
Stef Bastiaansen <stef@megasnort.com>
2024-12-26 20:59:38 +0100

    Better delete

--- bash_profile/bash.sh
+++ bash_profile/bash.sh
@@ -27,6 +27,6 @@ function ls() {
 export PS1="\[\033[33m\]\u\[\033[31m\]@\h\[\033[00m\] \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\]$ "
 
 function backup() {
-    rsync --delete -avxP /media/stef/fotos/fotos/ /media/stef/timeshift/fotos/
-	rsync --delete --exclude Downloads --exclude .cache --exclude .local --exclude .config --exclude .cache -avxP ~ /media/stef/timeshift/ds9/
+    rsync --delete-before -avxP /media/stef/fotos/fotos/ /media/stef/timeshift/fotos/
+	rsync --delete-before --exclude Downloads --exclude .mozilla --exclude .dbus --exclude .steam --exclude .cache --exclude .local --exclude .config --exclude .cache -avxP ~ /media/stef/timeshift/ds9/
 }
